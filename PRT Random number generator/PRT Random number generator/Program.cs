b72566c11ed6bca4d329d5afc097042ce98e90ad
Xoliswa <98959246+Xoliswa-Sitengile@users.noreply.github.com>
2022-04-06 01:03:31 +0200

    Add files via upload
    Generate random numbers from 1 to 100 with the size of 20

---
+++ PRT Random number generator/PRT Random number generator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRT_Random_number_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[20] { 24, 21, 17, 07, 05, 10, 21, 47, 51, 78, 90, 24, 99, 54, 87, 21, 17, 36, 40, 22 };
+            Random rnd = new Random();
+            /*numbers = rnd.Next();
+            for( int i = 0;i < numbers.Length; i++)
+            {
+                Console.WriteLine("{0}",numbers);
+            }*/
+
+            Console.ReadLine();
+        }
+       static void 
+    }
+}
